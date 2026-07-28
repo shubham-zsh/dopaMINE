a911114416ffd0970a4c8c2e8caaf39b10978bc6
Shubham Borse <shubhamborse0911@gmail.com>
2026-07-28 14:43:13 +0530

    added mongo connection to be

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,6 +5,10 @@ if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI is required in .env')
 }
 
+if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET is required in .env')
+}
+
 export const config = {
 
     mongoUri: process.env.MONGO_URI,
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,26 @@
 import express, { Request, Response } from "express"
+import { config } from './config.ts'
+import mongoose from "mongoose";
 
 const app = express()
 
-const port: number = 3000;
+async function start() {
+    try {
+        const connection = await mongoose.connect(config.mongoUri)
+        console.log("connection Successfull...")
+
+    } catch (err) {
+        console.error("connection failed...")
+        process.exit(1)
+    }
+}
+start()
 
 app.get('/ping', (req: Request, res: Response): void => {
     res.send("pong")
 });
 
-app.listen(port, () => {
+app.listen(config.port, () => {
     console.log("server running")
 })
 
@@ -16,9 +28,9 @@ app.post('/signup', async (req: Request, res: Response): Promise<void> => {
 
     const { email, password } = req.body;
 
-    if(!email && !password) {
+    if (!email && !password) {
         res.send("pls provide credentials")
         return;
     }
-    
+
 })
