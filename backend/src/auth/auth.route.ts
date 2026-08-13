3b092e1e7b1232ccfc2419381a60681dd7404051
Shubham Borse <shubhamborse0911@gmail.com>
2026-08-13 11:41:51 +0530

    created otp verify route

--- backend/src/auth/auth.route.ts
+++ backend/src/auth/auth.route.ts
@@ -30,7 +30,41 @@ router.post('/otp/request', async (req: Request, res: Response, next: NextFuncti
             return res.send("something went wrong")
         }
 
-        return res.send({msg: "otp generated succesfully", code})
+        return res.send({ msg: "otp generated succesfully", code })
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/otp/verify', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { otp, email } = req.body;
+
+        if (!otp || !email) {
+            return res.send("invalid credentials")
+        }
+
+        const getOtp = await OTP.findOne({ email, used: false })
+
+        if (!getOtp) {
+            return res.send("failed to fetch")
+        }
+
+        if (new Date > getOtp.expiresAt) {
+            return res.send("Otp is expired")
+        }
+
+        const isOtpTrue = await bcrypt.compare(otp, getOtp.hashedCode)
+
+        if (!isOtpTrue) {
+            return res.send("invalid otp")
+        }
+
+        getOtp.used = true;
+        await getOtp.save();
+
+        return res.send("logged in successfully")
 
     } catch (err) {
         next(err)
