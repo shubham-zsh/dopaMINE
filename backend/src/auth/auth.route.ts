46d7bf3787b761f56165d9a53260054a19b36f19
Shubham Borse <shubhamborse0911@gmail.com>
2026-08-13 10:33:11 +0530

    create otp generate route

--- backend/src/auth/auth.route.ts
+++ backend/src/auth/auth.route.ts
@@ -1,6 +1,40 @@
 import express, { NextFunction, Request, Response } from "express";
+import { OTP } from "../model/otp.ts";
+import bcrypt from 'bcrypt';
+
 const router = express.Router();
+
 
 router.post('/otp/request', async (req: Request, res: Response, next: NextFunction) => {
 
+    try {
+        const { email } = req.body;
+
+        if (!email) {
+            return res.send("please your email")
+        }
+
+        const code: number = Math.floor(Math.random() * 900000) + 100000;
+        console.log(code)
+
+        const hashed = await bcrypt.hash(String(code), 10)
+
+        const otpCreated = await OTP.create({
+            email: email,
+            hashedCode: hashed,
+            expiresAt: new Date(Date.now() + 10 * 60 * 1000),
+            used: false
+        })
+
+        if (!otpCreated) {
+            return res.send("something went wrong")
+        }
+
+        return res.send({msg: "otp generated succesfully", code})
+
+    } catch (err) {
+        next(err)
+    }
 })
+
+export default router
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,26 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
+import authRouter from './auth/auth.route.ts';
+import mongoose from "mongoose";
+import { config } from "./config.ts";
 
 const app = express()
+const port: number = 3004;
 
-const port: number = 3000;
+app.use(express.json())
+
+async function start() {
+    try {
+        const connection = await mongoose.connect(config.mongoUri)
+        console.log("connection Successfull...")
+
+    } catch (err) {
+        console.error("connection failed...")
+        process.exit(1)
+    }
+}
+start()
+
+app.use('/auth', authRouter)
 
 app.get('/ping', (req: Request, res: Response): void => {
     res.send("pong")
@@ -12,4 +30,3 @@ app.listen(port, () => {
     console.log("server running")
 })
 
-app.post('/otp', )
